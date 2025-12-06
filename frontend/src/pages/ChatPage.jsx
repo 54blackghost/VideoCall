d52458453a00b99061d1c8198dcb0a59edbcaaf6
54blackghost <ulrichleblack@gmail.com>
2025-12-06 19:24:43 +0100

    ajout du chatpage et envois du lien de videocall

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -18,6 +18,7 @@ import { StreamChat } from 'stream-chat';
 import toast from 'react-hot-toast';
 
 import ChatLoader from '../components/ChatLoader';
+import CallButton from '../components/CallButton';
 
 
 
@@ -79,7 +80,22 @@ const ChatPage = () => {
 
 
     initChat();
-  }, []);
+  }, [tokenData, authUser, targetUserId]);
+
+
+
+  const handleVideoCall = () => {
+    if (channel) {
+      const callUrl = `${window.location.origin}/call/${channel.id}`;
+
+
+      channel.sendMessage({
+        text: `I've started a video call. Join me here: ${callUrl}`,
+      });
+
+      toast.success('Video call link sent successfully!');
+    }
+  }
 
 
 
@@ -89,12 +105,14 @@ const ChatPage = () => {
        <Chat client={chatClient}>
         <Channel channel={channel}>
           <div className="w-full relative">
+            <CallButton handleVideoCall={handleVideoCall} />
             <Window>
               <ChannelHeader/>
                 <MessageList/>
                 <MessageInput focus/>
             </Window>
           </div>
+          <Thread/>
         </Channel>
        </Chat>
     </div>
